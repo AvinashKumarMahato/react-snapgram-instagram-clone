bbc54e0371fef4e6f98bc663615bda45563f319d
Avinash kumar Mahato <akr34206@gmail.com>
2024-04-06 12:49:22 +0530

    Update GridPostList.tsx

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -1,6 +1,6 @@
-import { useUserContext } from "@/context/AuthContext";
 import { Models } from "appwrite";
 import { Link } from "react-router-dom";
+import { useUserContext } from "@/context/AuthContext";
 import PostStats from "./PostStats";
 
 type GridPostListProps = {
@@ -15,10 +15,11 @@ const GridPostList = ({
   showStats = true,
 }: GridPostListProps) => {
   const { user } = useUserContext();
+
   return (
     <ul className="grid-container">
       {posts.map((post) => (
-        <li key={post.$id} className="relative min-w-80 h-80">
+        <li key={String(post.$id)} className="relative min-w-80 h-80">
           <Link to={`/posts/${post.$id}`} className="grid-post_link">
             <img
               src={post.imageUrl}
@@ -26,21 +27,19 @@ const GridPostList = ({
               className="h-full w-full object-cover"
             />
           </Link>
+
           <div className="grid-post_user">
             {showUser && (
               <div className="flex items-center justify-start gap-2 flex-1">
                 <img
-                  src={
-                    post.creator.imageUrl ||
-                    "/assets/icons/profile-placeholder.svg"
-                  }
+                  src={post.creator?.imageUrl || "/assets/icons/profile-placeholder.svg"}
                   alt="creator"
                   className="w-8 h-8 rounded-full"
                 />
-                <p className="line-clamp-1">{post.creator.name}</p>
+                <p className="line-clamp-1">{post.creator?.name}</p>
               </div>
             )}
-            {showStats && <PostStats post={post} userId={user.id} />}
+            {showStats && <PostStats post={post} userId={user?.id} />}
           </div>
         </li>
       ))}
